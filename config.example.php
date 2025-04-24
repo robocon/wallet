7068f20ecd63004fbd6db6e8eef1b5cfcbe9721a
Kritsanasak Kuntaros <roboconk@gmail.com>
2025-04-24 16:10:14 +0700

    fix

--- config.example.php
+++ config.example.php
@@ -4,3 +4,14 @@ define('PORT', 'PORT');
 define('DB', 'DATABASE_NAME');
 define('USER', 'DATABASE_USER');
 define('PASS', 'DATABASE_PASSWORD');
+
+$dbi = new mysqli(HOST, USER, PASS, DB, PORT);
+if ($dbi->connect_error) {
+    die("Connection failed: " . $dbi->connect_error);
+}
+
+function dump($t){
+    echo "<pre>";
+    print_r($t);
+    echo "</pre>";
+}
